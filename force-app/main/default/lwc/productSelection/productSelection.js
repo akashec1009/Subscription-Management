979f1e53f6d12d0b2cd3cabddcd36d1646a5e56e
akashec1009 <akashec1009@gmail.com>
2025-01-17 14:36:25 +0530

    add LWC

--- force-app/main/default/lwc/productSelection/productSelection.js
+++ force-app/main/default/lwc/productSelection/productSelection.js
@@ -1,3 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track} from 'lwc';
+import fetchProducts from '@salesforce/apex/c/productSelectionController.fetchProducts'
 
-export default class ProductSelection extends LightningElement {}
+export default class ProductSelection extends LightningElement {
+    @track searchTerm = '';
+    @track minPrice = null;
+    @track maxPrice = null;
+    @track minRating = null;
+    @track inStockOnly = false;
+    @track product = [];
+    @track isLoading = false;
+    @track error = null;
+
+    handleSearchChange(event){
+        this.searchTerm = event.target.value;
+    }
+    handleMinPriceChange(event){
+        this.minPrice = event.target.value ? parseFloat(event.target.value) : null;
+    }
+    handleMaxPriceChange(event){
+        this.maxPrice = event.target.value ? parseFloat(event.target.value) : null;
+    }
+    handleMinRatingChange(event){
+        this.minRating = event.target.value ? parseFloat(event.target.value) : null;
+    }
+    handleInStockToggle(event){
+        this.inStockOnly = event.target.checked;
+    }
+    fetchFilteredProduct(){
+        this.isLoading = true;
+        this.error = null;
+        fetchProducts({
+            searchTerm: this.searchTerm,
+            minPrice: this.minPrice,
+            maxPrice: this.maxPrice,
+            minRating: this.minRating,
+            inStockOnly:this.inStockOnly
+        })
+        .then((result) => {
+            this.product = result;
+            this.isLoading = false;
+        })
+        .catch((error) => {
+            this.error = error.body ? error.body.message : error.message;
+            this.isLoading = false;
+        });
+    }
+
+}
